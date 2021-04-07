426e9fc257ec5245df371b812058863f0c73fb3f
macSeoyun <newseoyun@gmail.com>
2021-04-08 00:06:55 +0900

    .

--- src/FactoryMethod/framework/Factory.java
+++ src/FactoryMethod/framework/Factory.java
@@ -9,4 +9,5 @@ public abstract class Factory {
     }
     protected abstract Product createProduct(String owner);
     protected abstract void registerProduct(Product product);
+
 }
--- src/FactoryMethod/framework/Product.java
+++ src/FactoryMethod/framework/Product.java
@@ -1,5 +1,8 @@
 package FactoryMethod.framework;
 
 public abstract class Product {
+
     public abstract void use();
+
 }
+
