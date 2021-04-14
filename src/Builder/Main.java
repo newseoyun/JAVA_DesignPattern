4514307729ad6449cc6594021e5022dda03d4f60
macSeoyun <newseoyun@gmail.com>
2021-04-14 22:55:00 +0900

    .

---
+++ src/Builder/Main.java
@@ -0,0 +1,36 @@
+package Builder;
+
+public class Main {
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            usage();
+            System.exit(0);
+        }
+
+        if (args[0].equals("plain")) {
+            TextBuilder textBuilder = new TextBuilder();
+            Director director = new Director(textBuilder);
+            director.construct();
+
+            String result = textBuilder.getResult();
+            System.out.println(result);
+        } else if (args[0].equals("html")) {
+            HTMLBuilder htmlBuilder = new HTMLBuilder();
+            Director director = new Director(htmlBuilder);
+            director.construct();
+
+            String fileName = htmlBuilder.getResult();
+
+            System.out.println(fileName + "가 작성되었습니다.");
+        } else {
+            usage();
+            System.exit(0);
+        }
+
+    }
+
+    private static void usage() {
+        System.out.println("Usage: Java Main plain 일반 텍스트로 문서작성");
+        System.out.println("Usage: Java Main html HTML 파일로 문서작성");
+    }
+}
---
+++ src/Builder/README.md
@@ -0,0 +1,6 @@
+# Builder Pattern
+
+***
+>
+>
+***
